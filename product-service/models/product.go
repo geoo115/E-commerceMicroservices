f465deb3d54957ae0dce45e646066a77548c10d1
george <george@George.GeOrGe>
2025-02-02 13:32:23 +0000

    add proto&models

---
+++ product-service/models/product.go
@@ -0,0 +1,35 @@
+package models
+
+import "gorm.io/gorm"
+
+type Category struct {
+	gorm.Model
+	Name     string    `json:"name" gorm:"unique"`
+	Products []Product `gorm:"foreignKey:CategoryID"`
+}
+
+type Product struct {
+	gorm.Model
+	Name        string    `json:"name"`
+	Price       float64   `json:"price"`
+	CategoryID  uint      `json:"category_id"`
+	Description string    `json:"description"`
+	Category    Category  `json:"category" gorm:"foreignKey:CategoryID"`
+	Inventory   Inventory `json:"inventory" gorm:"foreignKey:ProductID"`
+	Reviews     []Review  `json:"reviews" gorm:"foreignKey:ProductID"`
+}
+
+type Inventory struct {
+	gorm.Model
+	ProductID uint     `json:"product_id"`
+	Stock     int      `json:"stock"`
+	Product   *Product `json:"-" gorm:"foreignKey:ProductID"` // Use pointer to break recursion
+}
+
+type Review struct {
+	gorm.Model
+	ProductID uint   `json:"product_id"`
+	UserID    uint   `json:"user_id"`
+	Rating    int    `json:"rating" gorm:"check:rating >= 1 AND rating <= 5"`
+	Comment   string `json:"comment"`
+}
