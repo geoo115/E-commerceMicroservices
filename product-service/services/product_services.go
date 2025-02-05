3ea9bebf53411c35b30024b22003a4ccda46fe65
george <george@George.GeOrGe>
2025-02-05 16:11:33 +0000

    fix: update module path

--- product-service/services/product_services.go
+++ product-service/services/product_services.go
@@ -6,9 +6,10 @@ import (
 	"errors"
 	"fmt"
 
+	"github.com/geoo115/E-commerceMicroservices/product-service/product-service/proto"
+
 	"github.com/geoo115/E-commerceMicroservices/product-service/db"
 	"github.com/geoo115/E-commerceMicroservices/product-service/models"
-	"github.com/geoo115/E-commerceMicroservices/product-service/proto"
 )
 
 type ProductServer struct {
--- product-service/tests/product_service_test.go
+++ product-service/tests/product_service_test.go
@@ -11,7 +11,7 @@ import (
 
 	"github.com/geoo115/E-commerceMicroservices/product-service/db"
 	"github.com/geoo115/E-commerceMicroservices/product-service/models"
-	pb "github.com/geoo115/E-commerceMicroservices/product-service/proto"
+	pb "github.com/geoo115/E-commerceMicroservices/product-service/product-service/proto"
 	"github.com/geoo115/E-commerceMicroservices/product-service/services"
 )
 
